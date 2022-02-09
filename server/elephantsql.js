d552a161810daa88926cbe7d3d8fc53a54a4acba
migeuu <miguelmarkiori@gmail.com>
2022-02-09 11:41:58 -0300

    elephantsql connection

---
+++ server/elephantsql.js
@@ -0,0 +1,18 @@
+var pg = require('pg');
+var client = new pg.Client("postgres:/.elephantsql.com:The Actual url");
+
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+  });
+
+});
+
+module.exports = client;
