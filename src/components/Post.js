b7d252ca3aa813b2280c479414ddf03d21b565c8
migeuu <miguelmarkiori@gmail.com>
2022-02-09 11:42:30 -0300

    Post component update

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,39 @@
 import React from "react";
-import { FaEllipsisV } from "react-icons/fa";
 
 const Post = (props) => {
   return (
-    <div className="w-screen min-h-[50vh] bg-white shadow-xl">
+    <div className="lg:w-1/2 lg:rounded-xl mx-auto w-full min-h-[50vh] bg-white shadow-xl mb-4">
       <header className="flex items-center justify-between p-4 border-b-2">
         <h1 className="font-bold text-xl">{props.title}</h1>
-        <FaEllipsisV className="hover:text-blue-400 transition duration-300" />
+        <div className="flex justify-center items-center hover:text-blue-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+            />
+          </svg>
+        </div>
       </header>
-      <img src={props.image} alt="Imagem do post" className="w-full bg-black min-h-[50vh]" />
+      <img
+        src={props.image}
+        alt="Imagem do post"
+        className="w-full bg-black min-h-[50vh]"
+      />
       <div className="p-4 text-gray-600">
         <p>{props.description}</p>
         <div className="flex">
           <span>
             <span className="font-bold">Criado:</span> {props.datestart}
           </span>
+
           <span>
             <span className="font-bold">Até:</span> {props.dateend}
           </span>
