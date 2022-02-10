1679b4584a920c5797accc658c53ea04cadb695f
migeuu <miguelmarkiori@gmail.com>
2022-02-10 12:01:22 -0300

    Posts page

---
+++ src/components/Anuncios.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "./Post";
+
+const Anuncios = () => {
+  return (
+    <>
+      <Post title="Olá marilene" description="Ford Fiesta 2015" />
+    </>
+  );
+};
+
+export default Anuncios;
