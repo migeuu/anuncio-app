23ade0b87753ac3e77dd4a4d827e4c9195ef3122
migeuu <miguelmarkiori@gmail.com>
2022-02-10 12:00:07 -0300

    commands to cmd

--- server/database.sql
+++ server/database.sql
@@ -2,11 +2,32 @@ CREATE DATABASE anuncios;
 
 CREATE TABLE anuncio (
   idanuncio SERIAL NOT NULL PRIMARY KEY,
-  titulo VARCHAR(100),
-  descricao VARCHAR(255),
-  data_inicial DATE,
-  data_final DATE,
-  categoria VARCHAR(50),
+  title VARCHAR(100),
+  description VARCHAR(255),
+  date_start DATE,
+  date_end DATE,
+  category VARCHAR(50),
   email VARCHAR(255),
-  telefone INT
+  phone INT
 );
+
+INSERT INTO
+  anuncio (
+    title,
+    description,
+    date_start,
+    date_end,
+    category,
+    email,
+    phone
+  )
+VALUES
+  (
+    'Honda Fit 2020',
+    'Carro zeradinho na faixa, precinho de banana!!',
+    '2022-02-10',
+    '2022=02-20',
+    'Automóveis',
+    'miguel@exemplo.com',
+    84290137
+  );
