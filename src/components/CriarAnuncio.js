1d727e154dd283bd550886a5d663c5189aa002e8
migeuu <miguelmarkiori@gmail.com>
2022-02-10 12:01:33 -0300

    CreatePost page

---
+++ src/components/CriarAnuncio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CriarAnuncio = () => {
+  return (
+    <div className="">
+      <p>oi estou aqui para criar anuncios</p>
+    </div>
+  )
+}
+
+export default CriarAnuncio
